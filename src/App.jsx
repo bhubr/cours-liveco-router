7c449d43759528a5720dd2bcad3e9e6f90940dbd
Benoit Hubert <benoit.hubert@wildcodeschool.com>
2020-10-27 14:17:44 +0100

    install react router and add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+// import { Route } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header title="React App" text="This is a test" />
+      <Home />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+function Contact() {
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input id="name" type="text" />
+      </label>
+    </form>
+  );
+}
+
+export default Contact;
